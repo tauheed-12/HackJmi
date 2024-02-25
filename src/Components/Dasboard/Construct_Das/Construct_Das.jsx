9a6daf7c2ca68b87617e55b2d4198deb7fefbcf6
tauheed-12 <mohammadtauheed160@gmail.com>
2024-02-25 16:15:13 +0530

    fifth commit

--- src/Components/Dasboard/Construct_Das/Construct_Das.jsx
+++ src/Components/Dasboard/Construct_Das/Construct_Das.jsx
@@ -1,30 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./Construct_Das.css";
-import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Const_Vedio from "../../../Assets/const_vedio.mp4"
 import img1 from "../../../Assets/Const_P1.jpg"
 import img2 from "../../../Assets/Const_P2.jpg"
 import img3 from "../../../Assets/Const_P3.jpg"
-import { Link } from 'react-router-dom';
 import img4 from "../../../Assets/Const_P4.jpg"
 import img5 from "../../../Assets/Const_P5.jpg"
 import img6 from "../../../Assets/Const_P6.jpg"
 import img7 from "../../../Assets/Const_P7.jpg"
 import const_label from "../../../Assets/Const_Label.jpg"
+
 const Construct_Das = () => {
-  const img = [img1, img2, img3, img4, img5, img6, img7]
+  const imgArray = [img1, img2, img3, img4, img5, img6, img7];
+  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+
+  const nextImage = () => {
+    setSelectedImageIndex((prevIndex) => (prevIndex + 1) % imgArray.length);
+  };
   const [selectedFile, setSelectedFile] = useState(null);
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
   };
   const handleUpload = () => {
-    // You can implement your upload logic here, e.g., using FormData and sending it to your server
     if (selectedFile) {
       const formData = new FormData();
       formData.append('file', selectedFile);
-      
-      // Send formData to your server using fetch or Axios
-      // Example using fetch:
       fetch('https://example.com/upload', {
         method: 'POST',
         body: formData
@@ -37,61 +38,67 @@ const Construct_Das = () => {
       });
     }
   };
+  useEffect(() => {
+    const intervalId = setInterval(nextImage, 2000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const selectedImage = imgArray[selectedImageIndex];
+
   return (
     <div className='safety__Dashboard-Construction'>
-    <div className='safety_button_form'>
-    <Link to='/construction-Live_stream'>
-    <div className='live__stream_button'>
-    <button type='submit'>Live Stream</button>
-    </div>
-    </Link>
-      <div className='upload_images'>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleUpload}>Upload</button>
-    </div>
-    </div>
+      <div className='safety_button_form'>
+        <Link to='/construction-Live_stream'>
+          <div className='live__stream_button'>
+            <button type='submit'>Live Stream</button>
+          </div>
+        </Link>
+        <div className='upload_images'>
+          <input type="file" onChange={handleFileChange} />
+          <button onClick={handleUpload}>Upload</button>
+        </div>
+      </div>
+      <div className='safety__Dashboard-Construction-vedio-image'>
         <div className='safety__Dashboard-Construction-vedio'>
-          <h1>Demo Video</h1>
-          <div className='dashc-head-help'></div>
+          <h1>Demo Vedio</h1>
           <video controls autoPlay>
             <source src={Const_Vedio} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
-        <div className='safety__Dashboard-Construction-OriginalImg'>
-          <h2>Model images</h2>
+         {/* <div className='safety__Dashboard-Construction-OriginalImg'>
           <div className='original-headc-help'></div>
-        </div>
+        </div>  */}
         <div className="safety__Dashboard-Construction-ModelImage">
-          {img.map((image, index) => (
-            <img key={index} src={image} alt={`Image ${index}`} />
-          ))}
+        <h2>Model images</h2>
+          <img src={selectedImage} alt={`Image ${selectedImageIndex}`} />
         </div>
-        <div className='safety__Dashboard-Construction-Class'>
+      </div>
+      <div className='safety__Dashboard-Construction-Class'>
         <div className='safety__Dashboard-Construction-ClassList'>
           <h2>Classes List</h2>
           <div className='classlist-headc-help'></div>
-             <ul>
-                <li>Hardhat</li>
-                <li>Mask</li>
-                <li>NO-Hardhat</li>
-                <li>N0-Mask</li>
-                <li>NO-Safety Vest</li>
-                <li>Person</li>
-                <li>Sefty Cone</li>
-                <li>Safety Vest</li>
-                <li>Machinery</li>
-                <li>Vehicle</li>
-             </ul>
+          <ul>
+            <li>Hardhat</li>
+            <li>Mask</li>
+            <li>NO-Hardhat</li>
+            <li>N0-Mask</li>
+            <li>NO-Safety Vest</li>
+            <li>Person</li>
+            <li>Sefty Cone</li>
+            <li>Safety Vest</li>
+            <li>Machinery</li>
+            <li>Vehicle</li>
+          </ul>
         </div>
         <div className='safety__Dashboard-Construction-Labels'>
           <h2>Labels Diagram</h2>
           <div className='labels-headc-help'></div>
-          <img src={const_label} alt='label-diagram'></img>
-        </div>
+          <img src={const_label} alt='label-diagram' />
         </div>
       </div>
-  )
+    </div>
+  );
 }
 
 export default Construct_Das;
--- src/Components/Dasboard/Road_Dash/Road_Dash.jsx
+++ src/Components/Dasboard/Road_Dash/Road_Dash.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import "./Road_Dash.css"
 import vedio_road from "../../../Assets/Road_Vedio.mp4";
 import Road_P1 from "../../../Assets/Road_P1.jpg";
@@ -10,7 +10,12 @@ import Road_P4 from "../../../Assets/Road_P4.jpg";
 import Road_P5 from "../../../Assets/Road_P5.jpg";
 import road_labels from "../../../Assets/road_labels.jpg"
 const Road_Dash = () => {
-  const img = [Road_P1, Road_P2, Road_P3, Road_P4, Road_P5];
+  const imgArray = [Road_P1, Road_P2, Road_P3, Road_P4, Road_P5];
+  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+
+  const nextImage = () => {
+    setSelectedImageIndex((prevIndex) => (prevIndex + 1) % imgArray.length);
+  };
   const [selectedFile, setSelectedFile] = useState(null);
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
@@ -31,9 +36,14 @@ const Road_Dash = () => {
       });
     }
   };
+  useEffect(() => {
+    const intervalId = setInterval(nextImage, 2000);
+    return () => clearInterval(intervalId);
+  }, []);
+  const selectedImage = imgArray[selectedImageIndex];
   return (
-      <div className='safety__Dashboard-Road'>
-      <div className='safety_button_form'>
+    <div className='safety__Dashboard-Road'>
+    <div className='safety_button_form'>
     <Link to='/road-Live_stream'>
     <div className='live__stream_button'>
     <button type='submit'>Live Stream</button>
@@ -44,22 +54,18 @@ const Road_Dash = () => {
       <button onClick={handleUpload}>Upload</button>
     </div>
     </div>
+      <div className='safety__Dashboard-Road-vedio-image'>
         <div className='safety__Dashboard-Road-vedio'>
-          <h1>Demo Video</h1>
-          <div className='dash-head-help'></div>
+         <h1>Demo Video</h1>
           <video controls autoPlay>
             <source src={vedio_road} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
-        <div className='safety__Dashboard-Road-OriginalImg'>
-          <h2>Model images</h2>
-          <div className='original-head-help'></div>
-        </div>
         <div className="safety__Dashboard-Road-ModelImage">
-          {img.map((image, index) => (
-            <img key={index} src={image} alt={`Image ${index}`} />
-          ))}
+        <h1>Model Image</h1>
+        <img src={selectedImage} alt={`Image ${selectedImageIndex}`} />
+        </div>
         </div>
         <div className='safety__Dashboard-Class'>
         <div className='safety__Dashboard-Road-ClassList'>
